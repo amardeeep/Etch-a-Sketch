fc58182e266fa7dfc21d5e5569135877024038d6
Amardeep <amardeepdeepak2016@gmail.com>
2024-01-09 22:17:06 +0530

    Finished applying flex to divs and adding styles added styles to various divs and completed making function to create grid.

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 function createDivs(numberDivs){
-    for(let i=0;i<numberDivs;i++){
     const cont = document.querySelector('.container');
-    const divs = document.createElement('div');
-    divs.textContent='Div '+ (i+1);
-    divs.setAttribute('height','5em');
-    divs.setAttribute('width', '5em');
-    cont.appendChild(divs);
+    for(let i=0;i<numberDivs;i++){
+        const divsColumn = document.createElement('div');
+        for(let j=0;j<numberDivs;j++){
+            
+            const divsRow = document.createElement('div');
+            
+            divsRow.style.height='1em';
+            divsRow.style.flex='1';
+            divsRow.style.width='1em';
+            divsRow.style.aspectRatio='1';
+            divsRow.style.borderStyle='solid';
+            divsRow.style.borderColor='black';
+            divsColumn.appendChild(divsRow);
+        }
+        divsColumn.style.display='flex';
+        divsColumn.style.flex='1';
+        divsColumn.style.flexDirection='row';
+        divsColumn.style.alignItems='centre';
+        divsColumn.style.justifyContent='space-evenly';
+        cont.appendChild(divsColumn);
+
     }
 }
 createDivs(16);
