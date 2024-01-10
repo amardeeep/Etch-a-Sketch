ee99eb2ddb3138c233eec23098dc5a66c918c5a5
Amardeep <amardeepdeepak2016@gmail.com>
2024-01-10 12:02:06 +0530

    Add Button to change grid size added a button that removes old grid and replaces it with a new one

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Etch-a-Sketch
+This is my first attempt at making this Etch-A-Sketch. I have added hover effect to squares that changes its color to black
+and a button that asks user to input size of grid, which replaces the old grid with a new one with the mentioned dimensions.
+I would add clear and eraser functions as well, is some time. Also a lot of fine tuneing is needed.
--- main.js
+++ main.js
@@ -1,11 +1,13 @@
+const cont = document.querySelector('.container');
 function createDivs(numberDivs){
-    const cont = document.querySelector('.container');
+    
     for(let i=0;i<numberDivs;i++){
         const divsColumn = document.createElement('div');
         for(let j=0;j<numberDivs;j++){
             
             const divsRow = document.createElement('div');
             divsRow.classList.add('hover');
+            divsRow.classList.add('grid');
             divsRow.style.height='1em';
             divsRow.style.flex='1';
             divsRow.style.width='1em';
@@ -34,4 +36,29 @@ function hoverDivs(){
     }); 
 }
 hoverDivs();
+
+//provide function to clear
+
+//provide function to eraser
+
+//provide function to change grid dimensions button
+function editGrid(){
+    const newDimensionOfGrid=Number(prompt('Please enter the number of squares at a side-',16));
+    const element=document.getElementById('cont');
+    while(element.firstChild){
+        element.removeChild(element.firstChild);
+    }
+    
+    if(newDimensionOfGrid&&newDimensionOfGrid<100){
+        createDivs(newDimensionOfGrid);
+        hoverDivs();
+    }
+    else{
+        createDivs(16);
+        hoverDivs();
+    }
+    
+}
+const btnGrid= document.querySelector('.grid');
+btnGrid.addEventListener('click',editGrid);
 
